6e41f012890a80f690263cf76b12d94ea5d66d3b
infidelus <39056969+infidelus@users.noreply.github.com>
2022-06-06 10:39:19 +0100

    New text file check meethod
    Also added some ANSI escape codes to give some colour for terminal ouput

--- vrdwatch.py
+++ vrdwatch.py
@@ -15,6 +15,27 @@ ARG3 = "--vdpau"
 ARG4 = "--ini=comskip.ini"
 ARG5 = "--output=/comskip/output/folder"
 PROCESSED_FILES = "processed.txt"
+IGNORED_FILES = "ignore_list.txt"
+
+
+class TerminalColours:
+    """ANSI Escape Codes to add text colours in the terminal to make it easier to see what's going on"""
+    GREEN = "\u001b[32m"
+    BLUE = "\x1B[34m"
+    MAGENTA = "\x1B[35m"
+    YELLOW = "\x1B[33m"
+    RED = "\x1B[31m"
+    RESET = "\u001b[0m"
+
+
+def text_file_check():
+    """Check to see if the processed and ignored text files exist; create them if they don't"""
+    text_files = [PROCESSED_FILES, IGNORED_FILES]
+    for text_file in text_files:
+        if os.path.exists(text_file):
+            return True
+        else:
+            os.mknod(text_file)
 
 
 def ignored_shows(filename):
@@ -29,13 +50,8 @@ def ignored_shows(filename):
 
 def check_processed():
     """Checks to see if we have already processed the recording and added it to processed.txt"""
-    try:
-        with open(PROCESSED_FILES, "r") as pf:
-            file_check = pf.readlines()
-    except FileNotFoundError:
-        with open(PROCESSED_FILES, "w+") as pf:
-            file_check = pf.readlines()
-    else:
+    with open(PROCESSED_FILES, "r") as pf:
+        file_check = pf.readlines()
         for entry in file_check:
             if str(file) in entry:
                 return True
@@ -44,7 +60,7 @@ def check_processed():
 
 def on_disk(filename):
     """Checks the files on disk"""
-    if HD_PROGRAMS in filename:
+    if HIGH_DEF in filename:
         for vid in os.listdir(VIDEOS):
             if vid == filename:
                 return True
@@ -58,7 +74,7 @@ def on_disk(filename):
 
 def file_size_check(video):
     """Returns the file size for each video"""
-    if HD_PROGRAMS in video:
+    if HIGH_DEF in video:
         return os.stat(os.path.join(VIDEOS, video)).st_size
     else:
         return os.stat(os.path.join(RECORDINGS, video)).st_size
@@ -66,14 +82,14 @@ def file_size_check(video):
 
 def is_recording(video):
     """Checks to see if the recording is still in process; ignores it if it is"""
-    print("Checking file size to see if recording is still active.")
+    print(f"{TerminalColours.GREEN}Checking {video}'s file size to see if recording is still active.{TerminalColours.RESET}")
     first_check = file_size_check(video)
-    print(f"First Check - current file size is {first_check}")
+    print(f"{TerminalColours.MAGENTA}First Check - current file size is {first_check}")
     sleep(5)
     second_check = file_size_check(video)
-    print(f"Second Check - current file size is {second_check}")
+    print(f"Second Check - current file size is {second_check}{TerminalColours.RESET}")
     if second_check > first_check:
-        print(f"{video} still recording or QSF running; skipping for now")
+        print(f"{TerminalColours.YELLOW}{video} is still recording or QSF running; skipping for now{TerminalColours.RESET}")
         return True
     else:
         return False
@@ -101,42 +117,43 @@ def processed_exist_check():
                         removing.write(line)
 
 
+text_file_check()
 processed_exist_check()
 
 # SD Recordings
-print("Checking recordings folder for new files.\n")
+print(f"{TerminalColours.GREEN}Checking recordings folder for new files.\n{TerminalColours.RESET}")
 for file in os.listdir(RECORDINGS):
     if fnmatch.fnmatch(file, "*.ts"):
-        if ignored_shows(file):
-            print(f"{file} is in the ignore list.  Not processing.")
-            continue
         if check_processed():
             continue
-        if HD_PROGRAMS in file:
-            print(f"{file} is a HD recording.  QSF required before processing.\n")
-            continue  # We skip HD files here so we can QSF them first.  Will be picked up in the HD section
+        if ignored_shows(file):
+            print(f"{TerminalColours.RED}{file} is in the ignore list.  Not processing.{TerminalColours.RESET}")
+            continue
+        if HIGH_DEF in file:
+            print(f"{TerminalColours.YELLOW}{file} is a HD recording.  QSF required before processing.\n{TerminalColours.RESET}")
+            continue
         if is_recording(file):
             continue
         else:
-            print(f"Processing {file}")
+            print(f"{TerminalColours.BLUE}Processing {file}{TerminalColours.RESET}")
             result = subprocess.run([COMSKIP, ARG1, ARG2, ARG3, ARG4, ARG5, f"{RECORDINGS}{file}"])
             with open(PROCESSED_FILES, "a") as completed:
                 completed.write(f"{file}\n")
-                print("Deleting redundant comskip files.\n")
+                print(f"{TerminalColours.GREEN}Deleting redundant comskip files.\n{TerminalColours.RESET}")
                 delete_extra_files()
 
 # HD Recordings
-print("Checking for new HD recordings.\n")
+print(f"{TerminalColours.GREEN}Checking for new HD recordings.\n{TerminalColours.RESET}")
 for file in os.listdir(VIDEOS):
-    if HD_PROGRAMS in file and fnmatch.fnmatch(file, "*.ts"):
+    if HIGH_DEF in file and fnmatch.fnmatch(file, "*.ts"):
         if check_processed():
             continue
         if is_recording(file):
             continue
         else:
-            print(f"Processing {file}")
+            print(f"{TerminalColours.BLUE}Processing {file}{TerminalColours.RESET}")
             result = subprocess.run([COMSKIP, ARG1, ARG2, ARG3, ARG4, ARG5, f"{VIDEOS}{file}"])
             with open(PROCESSED_FILES, "a") as completed:
                 completed.write(f"{file}\n")
-                print("Deleting redundant comskip files.\n")
+                print(f"{TerminalColours.GREEN}Deleting redundant comskip files.\n{TerminalColours.RESET}")
                 delete_extra_files()
